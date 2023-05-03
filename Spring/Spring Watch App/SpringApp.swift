bd900f818da9fecaf69eef936d20ab83601abf85
angry-dev <98198645+angry-dev@users.noreply.github.com>
2023-05-03 15:25:17 +0900

    최초 커밋

---
+++ Spring/Spring Watch App/SpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringApp.swift
+//  Spring Watch App
+//
+//  Created by Hyesung Jeon on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct Spring_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Spring/Spring/SpringApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpringApp.swift
+//  Spring
+//
+//  Created by Hyesung Jeon on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct SpringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
